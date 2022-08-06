f03e1ca9fe2c53a1d05f39643164b2cf3aa83587
casper <casper@DESKTOP-2PU1K7L>
2022-08-06 13:44:56 +0300

    Add project files.

---
+++ DashboardLinqVeriCekme/Controllers/defaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DashboardLinqVeriCekme.Models.Entity;
+
+namespace DashboardLinqVeriCekme.Controllers
+{
+    public class defaultController : Controller
+    {
+        UrunTakipDBEntities db = new UrunTakipDBEntities();
+        // GET: default
+        public ActionResult Index()
+        {
+            var kategori = db.Kategori.Count();
+            ViewBag.ktgr = kategori;
+
+            var musteri = db.Musteri.Count();
+            ViewBag.mst = musteri;
+
+            var urun = db.Urun.Count();
+            ViewBag.urn = urun;
+
+            var satis = db.Satis.Count();
+            ViewBag.sts = satis;
+
+            return View();
+        }
+    }
+}
